8a0c8d440220e1470fda0587c8957735d9966987
luidy roecker duessmann <luidyduessmann68@gmail.com>
2026-08-20 15:58:00 -0300

    so o filme

--- app/(tabs)/filme.tsx
+++ app/(tabs)/filme.tsx
@@ -1,9 +1,17 @@
-import {View, StyleSheet, FlatList, Image} from 'react-native';
+import {View, StyleSheet, FlatList, Image, TextInput} from 'react-native';
 
 export default function App() {
     return (
         <View style={styles.container}>
-            
+            <View style={styles.titulo}>
+            titulo do filme
+            </View>
+            <View style={styles.filme}>
+            capa do filme
+            </View>
+            <View style={styles.descricao}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis, soluta. Rem tenetur ipsam totam quaerat quam? Eos, impedit aut quas mollitia tenetur deserunt sed quidem dignissimos, tempora praesentium reiciendis eaque.
+            </View>
         </View>
     );
 }
@@ -12,6 +20,26 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         backgroundColor:"#000",
-        
+    },
+    filme:{
+        width:400,
+        height:400,
+        backgroundColor: "#2e2e2e",
+        color: "#fff",
+        alignItems:"center",
+        alignSelf:'center',
+    },
+    descricao:{
+        width:400,
+        height:100,
+        color:'#fff',
+        alignItems:'center',
+        alignSelf:'center',
+        margin: 10
+    },
+    titulo:{
+        color:'#fff',
+        fontSize: 25,
+        margin: 10
     },
 });
